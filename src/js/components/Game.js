2e7d730b1b360f896bc9478b7d86b68ee289d8ab
Artur <arko8919@gmail.com>
2022-12-29 14:17:40 +0100

    base html structure

--- src/js/components/Game.js
+++ src/js/components/Game.js
@@ -1,5 +1,22 @@
-export default function Game() {
-  return (
-    <div>Game</div>
-  )
+import React from "react";
+import Board from "./Board";
+
+import "./game.scss"
+
+export default class Game extends React.Component {
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <section className="game d-flex flex-row">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </section>
+    )
+  }
 }
